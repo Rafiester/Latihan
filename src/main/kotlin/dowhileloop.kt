37f31691295c85437b0efd9b14f683e75d8ab624
Rafiester <52811828+Damar131@users.noreply.github.com>
2022-03-21 12:25:20 +0700

    kotlin: tugas 1 dari w3schools

---
+++ src/main/kotlin/dowhileloop.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var i = 0;
+    do {
+        println(i)
+        i++
+    }
+    while (i < 5)  //value yang dikeluarkan sama seperti whileloop yaitu value kurang dari 5, namun kali ini dimulai dr 0
+}
